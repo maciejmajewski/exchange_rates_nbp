f7d01c5f71b9d05e45b4130d63e8b9551f26e9f5
dolphik <dolphik@gmail.com>
2018-12-13 19:11:07 +0100

    Adding tests for table_number and table_date

--- spec/exchange_rates_nbp_spec.rb
+++ spec/exchange_rates_nbp_spec.rb
@@ -17,4 +17,29 @@ describe ExchangeRatesNBP do
       expect(exchange_rate).to eq(3.3524)
     end
   end
+
+  describe '#exchange_table_number' do
+    subject(:exchange_table_number) do
+      described_class.exchange_table_number(date)
+    end
+
+    let(:date) { Date.new(2016, 2, 1) }
+
+    it 'fetches currency table number', :vcr do
+      expect(exchange_table_number).to eq("020/A/NBP/2016")
+    end
+  end  
+
+  describe '#exchange_table_date' do
+    subject(:publish_date) do
+      described_class.exchange_table_date(date)
+    end
+
+    let(:date) { Date.new(2016, 2, 7) }
+
+    it 'fetches currency table publish date', :vcr do
+      expect(publish_date).to eq( Date.new(2016, 2, 5) )
+    end
+  end 
+
 end
